5e10a057a6c77d817c175c5d8930b225d874f4c6
Ouiamebenmalek <ouiame1benmalek@gmail.com>
2023-02-27 03:21:51 +0100

    new

--- front/http-common.ts
+++ front/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://backendpokemon-production.up.railway.app/",
   headers: {
     "Content-type": "application/json"
   }
